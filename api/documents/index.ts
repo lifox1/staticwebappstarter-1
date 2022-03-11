b8f1d8c99feefb43b552b9f7bb98ad88a8ba2529
James Hoff <jameshoff@microsoft.com>
2022-03-11 10:25:10 -0500

    update

--- api/documents/index.ts
+++ api/documents/index.ts
@@ -1,31 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import formidable from 'formidable';
-const { BlobServiceClient } = require('@azure/storage-blob');
 import * as fs from 'fs';
-
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
-
-const upload2BlobStorage = async (filename) => {
-
-    // Create the BlobServiceClient object which will be used to create a container client
-    const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.BLOB_STORAGE_CONNECTION_STRING);
-    // Get a reference to a container
-    const containerClient = blobServiceClient.getContainerClient(process.env.BLOB_STORAGE_CONTAINER);
-    // Get a block blob client
-    const filenameSplit = filename.split("/")
-    const zipFilename = filenameSplit[filenameSplit.length - 1]
-    const blockBlobClient = containerClient.getBlockBlobClient(zipFilename);
-    const buf = fs.readFileSync(filename)
-    const uploadBlobResponse = await blockBlobClient.upload(buf, buf.length);
-    console.log("Blob was uploaded successfully. requestId: ", uploadBlobResponse.requestId);
-
-    return true
-}
-
+import { upload2BlobStorage } from "./uploadBlob";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     console.log(`${JSON.stringify(req.query)}`)
